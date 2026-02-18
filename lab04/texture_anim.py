4eb8c504ee456fcb80044c61cbad2da283759ed5
Daniel Jeong <danielyj147@gmail.com>
2026-02-18 12:35:42 -0500

    fix: pyray import

--- lab04/texture_anim.py
+++ lab04/texture_anim.py
@@ -8,7 +8,7 @@ Copyright (c) 2014-2025 Ramon Santamaria (@raysan5)
 This source has been converted from C raylib examples to Python.
 """
 
-from pyray import *
+import pyray
 from pathlib import Path
 
 THIS_DIR = Path(__file__).resolve().parent
@@ -20,24 +20,24 @@ MIN_FRAME_SPEED = 1
 screenWidth = 800
 screenHeight = 450
 
-init_window(screenWidth, screenHeight, "raylib [texture] example - sprite anim")
+pyray.init_window(screenWidth, screenHeight, "raylib [texture] example - sprite anim")
 
 # Important NOTE: Textures MUST be loaded after Window initialization 
 # (OpenGL context is required)
 # Texture loading: one step vs. two steps (see other example)
-scarfy = load_texture(str(THIS_DIR/"resources/scarfy.png"))  
+scarfy = pyray.load_texture(str(THIS_DIR/"resources/scarfy.png"))  
 
-position = Vector2(350.0, 280.0)
-frameRec = Rectangle(0.0, 0.0, float(scarfy.width)/6, float(scarfy.height))
+position = pyray.Vector2(350.0, 280.0)
+frameRec = pyray.Rectangle(0.0, 0.0, float(scarfy.width)/6, float(scarfy.height))
 currentFrame = 0
 
 framesCounter = 0
 framesSpeed = 8  # Number of spritesheet frames shown by second
 
-set_target_fps(60)  # Set our game to run at 60 frames-per-second
+pyray.set_target_fps(60)  # Set our game to run at 60 frames-per-second
 
 # Main game loop
-while not window_should_close():  # Detect window close button or ESC key
+while not pyray.window_should_close():  # Detect window close button or ESC key
     # Update
     framesCounter += 1
 
@@ -51,9 +51,9 @@ while not window_should_close():  # Detect window close button or ESC key
         frameRec.x = float(currentFrame) * float(scarfy.width)/6
 
     # Control frames speed
-    if is_key_pressed(KEY_RIGHT):
+    if pyray.is_key_pressed(pyray.KeyboardKey.KEY_RIGHT):
         framesSpeed += 1
-    elif is_key_pressed(KEY_LEFT):
+    elif pyray.is_key_pressed(pyray.KeyboardKey.KEY_LEFT):
         framesSpeed -= 1
 
     if framesSpeed > MAX_FRAME_SPEED:
@@ -62,32 +62,32 @@ while not window_should_close():  # Detect window close button or ESC key
         framesSpeed = MIN_FRAME_SPEED
 
     # Draw
-    begin_drawing()
+    pyray.begin_drawing()
     
-    clear_background(RAYWHITE)
+    pyray.clear_background(pyray.RAYWHITE)
     
-    draw_texture(scarfy, 15, 40, WHITE)
-    draw_rectangle_lines(15, 40, scarfy.width, scarfy.height, LIME)
-    draw_rectangle_lines(15 + int(frameRec.x), 40 + int(frameRec.y), 
-                           int(frameRec.width), int(frameRec.height), RED)
+    pyray.draw_texture(scarfy, 15, 40, pyray.WHITE)
+    pyray.draw_rectangle_lines(15, 40, scarfy.width, scarfy.height, pyray.LIME)
+    pyray.draw_rectangle_lines(15 + int(frameRec.x), 40 + int(frameRec.y), 
+                           int(frameRec.width), int(frameRec.height), pyray.RED)
     
-    draw_text("FRAME SPEED: ", 165, 210, 10, DARKGRAY)
-    draw_text(f"{framesSpeed:02d} FPS", 575, 210, 10, DARKGRAY)
-    draw_text("PRESS RIGHT/LEFT KEYS to CHANGE SPEED!", 290, 240, 10, 
-    DARKGRAY)
+    pyray.draw_text("FRAME SPEED: ", 165, 210, 10, pyray.DARKGRAY)
+    pyray.draw_text(f"{framesSpeed:02d} FPS", 575, 210, 10, pyray.DARKGRAY)
+    pyray.draw_text("PRESS RIGHT/LEFT KEYS to CHANGE SPEED!", 290, 240, 10, 
+    pyray.DARKGRAY)
     
     for i in range(MAX_FRAME_SPEED):
         if i < framesSpeed:
-            draw_rectangle(250 + 21*i, 205, 20, 20, RED)
+            pyray.draw_rectangle(250 + 21*i, 205, 20, 20, pyray.RED)
         #draw_rectangle_lines(250 + 21*i, 205, 20, 20, MAROON)
 
     # Draw part of the texture
-    draw_texture_rec(scarfy, frameRec, position, WHITE)  
+    pyray.draw_texture_rec(scarfy, frameRec, position, pyray.WHITE)  
     
-    draw_text("(c) Scarfy sprite by Eiden Marsal", screenWidth - 200,
-               screenHeight - 20, 10, GRAY)
-    end_drawing()
+    pyray.draw_text("(c) Scarfy sprite by Eiden Marsal", screenWidth - 200,
+               screenHeight - 20, 10, pyray.GRAY)
+    pyray.end_drawing()
 
 # De-Initialization
-unload_texture(scarfy)  # Texture unloading
-close_window()  # Close window and OpenGL context
+pyray.unload_texture(scarfy)  # Texture unloading
+pyray.close_window()  # Close window and OpenGL context
